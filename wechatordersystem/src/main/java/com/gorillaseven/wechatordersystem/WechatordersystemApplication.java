79922e22d39af14ba4747f50637ab2b025902ae8
1135459639@qq.com <1135459639@qq.com>
2019-05-29 15:08:48 +0800

    Initial commit

---
+++ wechatordersystem/src/main/java/com/gorillaseven/wechatordersystem/WechatordersystemApplication.java
@@ -0,0 +1,13 @@
+package com.gorillaseven.wechatordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatordersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatordersystemApplication.class, args);
+	}
+
+}
---
+++ wechatordersystem/src/test/java/com/gorillaseven/wechatordersystem/WechatordersystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gorillaseven.wechatordersystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WechatordersystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
